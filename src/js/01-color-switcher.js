1aea600c2e0c8d92375946c41ed379b1eee78d4e
vlad9909 <gva9909@gmail.com>
2023-01-14 22:56:16 +0200

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop');
+const bodyRef = document.querySelector('body');
+let start = null;
+
+btnStart.addEventListener('click', go);
+btnStop.addEventListener('click', stop);
+function go() {
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  start = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stop() {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+  clearInterval(start);
+}
